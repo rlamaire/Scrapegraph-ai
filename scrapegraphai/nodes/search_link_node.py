009d44c03fbbae11a5f5a2f4a91ea0d4925faa32
Robbe Lamaire <robbe.lamaire@gmail.com>
2024-05-26 14:54:43 +0200

    changed output parser to CommaSeparatedListOutputParser

--- scrapegraphai/nodes/search_link_node.py
+++ scrapegraphai/nodes/search_link_node.py
@@ -6,12 +6,11 @@ SearchLinkNode Module
 from typing import List, Optional
 from tqdm import tqdm
 
+
 # Imports from Langchain
 from langchain.prompts import PromptTemplate
-from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.output_parsers import JsonOutputParser, CommaSeparatedListOutputParser
 from langchain_core.runnables import RunnableParallel
-
-from ..utils.logging import get_logger
 
 # Imports from the library
 from .base_node import BaseNode
@@ -34,19 +33,13 @@ class SearchLinkNode(BaseNode):
         node_name (str): The unique identifier name for the node, defaulting to "GenerateAnswer".
     """
 
-    def __init__(
-        self,
-        input: str,
-        output: List[str],
-        node_config: Optional[dict] = None,
-        node_name: str = "GenerateLinks",
-    ):
+    def __init__(self, input: str, output: List[str], node_config: Optional[dict] = None,
+                 node_name: str = "GenerateLinks"):
         super().__init__(node_name, "node", input, output, 1, node_config)
 
         self.llm_model = node_config["llm_model"]
-        self.verbose = (
-            False if node_config is None else node_config.get("verbose", False)
-        )
+        self.verbose = False if node_config is None else node_config.get(
+            "verbose", False)
 
     def execute(self, state: dict) -> dict:
         """
@@ -65,14 +58,16 @@ class SearchLinkNode(BaseNode):
                         necessary information for generating the answer is missing.
         """
 
-        self.logger.info(f"--- Executing {self.node_name} Node ---")
+        if self.verbose:
+            print(f"--- Executing {self.node_name} Node ---")
 
         # Interpret input keys based on the provided input expression
         input_keys = self.get_input_keys(state)
 
         user_prompt = state[input_keys[0]]
         parsed_content_chunks = state[input_keys[1]]
-        output_parser = JsonOutputParser()
+        # output_parser = JsonOutputParser()
+        output_parser = CommaSeparatedListOutputParser()
 
         prompt_relevant_links = """
             You are a website scraper and you have just scraped the following content from a website.
@@ -87,34 +82,20 @@ class SearchLinkNode(BaseNode):
             Please list only valid URLs and make sure to err on the side of inclusion if it's uncertain 
             whether the content at the link is directly relevant.
 
-            Output only a list of relevant links in the format:
-            [
-                "link1",
-                "link2",
-                "link3",
-                .
-                .
-                .
-            ]
+            Please follow the following format instructions: {format_instructions}
             """
         relevant_links = []
 
-        for i, chunk in enumerate(
-            tqdm(
-                parsed_content_chunks,
-                desc="Processing chunks",
-                disable=not self.verbose,
-            )
-        ):
+        for i, chunk in enumerate(tqdm(parsed_content_chunks, desc="Processing chunks", disable=not self.verbose)):
             merge_prompt = PromptTemplate(
                 template=prompt_relevant_links,
                 input_variables=["content", "user_prompt"],
+                partial_variables={'format_instructions': output_parser.get_format_instructions()},
             )
             merge_chain = merge_prompt | self.llm_model | output_parser
             # merge_chain = merge_prompt | self.llm_model
             answer = merge_chain.invoke(
-                {"content": chunk.page_content, "user_prompt": user_prompt}
-            )
+                {"content": chunk.page_content, "user_prompt": user_prompt})
             relevant_links += answer
         state.update({self.output[0]: relevant_links})
         return state
